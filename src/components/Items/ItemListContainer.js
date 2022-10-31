35404d903f93ac248b0e0c3e38f5c5645abece30
Leonardo Tricotti <tricottileo@gmail.com>
2022-10-31 14:33:22 -0300

    first commit

--- src/components/Items/ItemListContainer.js
+++ src/components/Items/ItemListContainer.js
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import { getProducts } from "../../assets/data/products";
+import styles from './items.module.css'
 
 export const Card = () => {
   const [productList, setProductList] = useState([]);
+
   useEffect(() => {
     getProducts().then((res) => setProductList(res));
   }, []);
+
   return (
     <div>
       {productList.map((item) => {
         return (
           <div key={item.id}>
-            <img src={item.img} alt={item.title} />
-            <p>{item.title}</p>
+            <img className= {styles.img} src={item.img} alt={item.title} />
+            <p className={styles.text}>{item.title}</p>
           </div>
         );
       })}
